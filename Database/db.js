8ffec7baf0a1bfd005c1afd1ce6961a9cdbedee4
Michael Nichol <mtdnichol@gmail.com>
2021-11-06 01:19:10 -0400

    Restful framework for users, posts, auth, and profile.  MongoDB connection instantiated as well

---
+++ Database/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true
+        })
+
+        console.log("MongoDB Connected...")
+    } catch (err) {
+        console.log(err.message)
+        // Exit process with failure
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
